b881bf8c4702ed76a54584d932ee7eb3c430a3f3
James Lamb <jameslamb13@gmail.com>
2020-11-03 00:31:33 -0500

    deleted about page and transfered contents to components.

---
+++ src/components/Resume/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Resume() {
+
+    return (
+       
+       
+       <a href="https://docs.google.com/document/d/1derMmx3P5p19w2RurNWZ1ZBbOSWU_UeQ7tJfaZKu0Os/export?format=pdf" style={{color: 'darkCyan'}}>Link to download my resume</a>
+
+    )
+}
+
+
+export default Resume;
