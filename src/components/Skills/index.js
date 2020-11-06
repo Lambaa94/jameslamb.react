775cf42c45df5a3c0db34e041790c6f4b197fc51
James Lamb <jameslamb13@gmail.com>
2020-11-06 01:52:07 -0500

    used raw=true on pictures.

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Row, Col} from "reactstrap";
 import "./style.css"
 
 function Skills() {
@@ -11,12 +12,14 @@ function Skills() {
         <br/>
         <br/>
         <ul>
-        <i className="fas fa-terminal fa-6x icons"></i>{" "}<i className="fab fa-html5 fa-6x" alt="html5"></i>{" "}<i className="fab fa-css3-alt fa-6x" alt="css3"></i>{" "}<i className="fab fa-bootstrap fa-6x"></i>{" "}<i className="fab fa-js-square fa-6x" alt="javscript"></i>{" "}<i className="fab fa-node fa-6x" alt="nodejs"></i>{" "}<i className="fab fa-react fa-6x"></i>{" "}<i className="fab fa-github fa-6x"></i>{" "}<i className="fab fa-git fa-6x"></i>{" "}<i className="fab fa-gitlab fa-6x"></i>
+            <Row>
+                <Col sm={12}>
+        <i className="fas fa-terminal fa-6x icons"></i>{" "}<i className="fab fa-html5 fa-6x" alt="html5"></i>{" "}<i className="fab fa-css3-alt fa-6x" alt="css3"></i>{" "}<i className="fab fa-bootstrap fa-6x"></i>{" "}<i className="fab fa-js-square fa-6x" alt="javscript"></i>{" "}<i className="fab fa-node fa-6x" alt="nodejs"></i>{" "}<i className="fab fa-react fa-6x"></i>{" "}<i className="fab fa-github fa-6x"></i>{" "}<i className="fab fa-git fa-6x"></i>{" "}<i className="fab fa-gitlab fa-6x"></i>{" "}<img className="icons" src={process.env.PUBLIC_URL + "/assets/icons/jquery.png"}></img><img className="icons" src={process.env.PUBLIC_URL + "/assets/icons/mongodb.png"}></img><img className="icons" src={process.env.PUBLIC_URL + "/assets/icons/npm.png"}></img>{" "}<img className="icons" src={process.env.PUBLIC_URL + "/assets/icons/express.png"}></img>{" "}<img className="icons" src={process.env.PUBLIC_URL + "/assets/icons/sequelize.png"}></img>
+          </Col>
+        </Row>
         </ul>
         <br/>
-        <ul>
-            {" "}<img className="icons" src={process.env.PUBLIC_URL + "/assets/icons/jquery.png"}></img><img className="icons" src={process.env.PUBLIC_URL + "/assets/icons/mongodb.png"}></img><img className="icons" src={process.env.PUBLIC_URL + "/assets/icons/npm.png"}></img>{" "}<img className="icons" src={process.env.PUBLIC_URL + "/assets/icons/express.png"}></img>{" "}<img className="icons" src={process.env.PUBLIC_URL + "/assets/icons/sequelize.png"}></img>
-        </ul>
+       
         </>
     )
 
