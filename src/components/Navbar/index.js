70895403227f85ce8f19a3fed2fdcfaed3dac7fa
James Lamb <jameslamb13@gmail.com>
2020-11-07 02:54:30 -0500

    added sticky topnav bar.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 function Navbar() {
     
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top sticky-top">
             <a className="navbar-brand">James Lamb</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
