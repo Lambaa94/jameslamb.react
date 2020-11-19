a1026348c3d111927482cf3caa27179d9ec025b8
James Lamb <jameslamb13@gmail.com>
2020-11-19 14:59:44 -0500

    added Table Turner to Projects in Portfolio. Css additions to contact square. MQ need assistance**.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -10,21 +10,24 @@ function Contact() {
         <hr/>
         <br/>
         <br/>
+        <Container>
         <Row >
             <Col>  
-         <a className="bottomIcons" id="resume" href="https://docs.google.com/document/d/1derMmx3P5p19w2RurNWZ1ZBbOSWU_UeQ7tJfaZKu0Os/export?format=pdf" style={{color: 'darkCyan'}}><i className="fas fa-file-pdf fa-8x m-3"></i></a>
+         <a className="bottomIcons" id="resume" href="https://docs.google.com/document/d/1derMmx3P5p19w2RurNWZ1ZBbOSWU_UeQ7tJfaZKu0Os/export?format=pdf" style={{color: 'darkCyan'}}><i className="fas fa-file-pdf fa-7x mt-2"></i></a>
         
             </Col>
             <Col>
-            <a href="tel:+1-646-853-1534"><i className="fas fa-mobile fa-8x bottomIcons m-3"></i></a> 
+            <a href="tel:+1-646-853-1534"><i className="fas fa-mobile fa-7x bottomIcons mt-2" ></i></a> 
             </Col>
+           
             <Col>
-            <a className="bottomIcons" href="https://github.com/Lambaa94" target="_blank"> <i className="fab fa-github-square fa-8x m-3"></i></a>
+            <a className="bottomIcons" href="https://github.com/Lambaa94" target="_blank"> <i className="fab fa-github-square fa-6x mt-2"></i></a>
             </Col>
-            <Col>
-            <a className="bottomIcons" href="mailto:jameslamb13@gmail.com"> <i className="fas fa-envelope-square fa-8x m-3"></i></a>
+            <Col >
+            <a className="bottomIcons" href="mailto:jameslamb13@gmail.com"> <i className="fas fa-envelope-square fa-6x mt-2" ></i></a>
             </Col>
             </Row >
+            </Container>
             <br/>
             <br/>
             <hr/>
