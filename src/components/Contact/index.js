16d84df655b1cf6795a52a5bfb1a5bce5eda7aa5
James Lamb <jameslamb13@gmail.com>
2021-03-01 13:20:18 -0500

    changed color of pdf icon and changed size to match others in the row.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -19,11 +19,11 @@ function Contact() {
                         <a className="bottomIcons" href="mailto:jameslamb13@gmail.com"> <i className="fas fa-envelope-square fa-6x mt-2" ></i></a>
                     </Col>
                     <Col>
-                        <a className="bottomIcons" id="resume" href="https://docs.google.com/document/d/1derMmx3P5p19w2RurNWZ1ZBbOSWU_UeQ7tJfaZKu0Os/export?format=pdf" style={{ color: 'darkCyan' }}><i className="fas fa-file-pdf fa-7x mt-2"></i></a>
+                        <a className="bottomIcons" id="resume" href="https://docs.google.com/document/d/1derMmx3P5p19w2RurNWZ1ZBbOSWU_UeQ7tJfaZKu0Os/export?format=pdf" style={{ color: '#00FFFF'}}><i className="fas fa-file-pdf fa-6x mt-2"></i></a>
 
                     </Col>
                     <Col>
-                        <a href="tel:+1-646-853-1534"><i className="fas fa-mobile fa-7x bottomIcons mt-2" ></i></a>
+                        <a href="tel:+1-646-853-1534"><i className="fas fa-mobile fa-6x bottomIcons mt-2" ></i></a>
                     </Col>
                 </Row >
             </Container>
