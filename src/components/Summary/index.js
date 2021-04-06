e8475e79ab05fcbb12e09b98dae4afd1151027a8
James Lamb <jameslamb13@gmail.com>
2021-04-06 16:32:27 -0400

    changed about section

--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -23,7 +23,7 @@ function Summary() {
                 <br />
                 <br />
 
-                <p> Full stack web developer with a long history in   Hospitality. Recently worked in a high intensity pre-theatre, fine dining restaurant dealing with large guest counts. I showed that I have exceptional time management skills combined with great organizational skills to keep my guests on time and comfortable. I'm passionate about creating and inventing new, never thought about products. Excited to bring my passion into the workspace with my newly found knowledge from the University of Pennsylvania's Coding Bootcamp. This is where I obtained my strong base for the MERN stack. Working my way through college and a conservatory taught me to keep my composure while under pressure. I am looking to be a part of a team to design and create web applications that will benefit the masses. The world is
+                <p> Full stack web developer with a long history in Hospitality. I'm passionate about creating and inventing new, never thought about products. Excited to bring my passion into the workspace with my knowledge from the University of Pennsylvania's Coding Bootcamp. This is where I obtained my strong base for the MERN stack. Working my way through college and a conservatory taught me to keep my composure while under pressure. I am looking to be a part of a team to design and create web applications that will benefit the masses. The world is
                 changing and advancing at a rapid pace, and it takes someone that is quick-witted and competent to get things done and done right.
                 </p>
                 <br />
